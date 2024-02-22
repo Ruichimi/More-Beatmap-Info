ad41471afa382abde67eeb51c48150e979e32dcb
Cadregich <thekeykeyloy@gmail.com>
2024-02-22 14:21:06 +0100

    Working with correct building

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import path from 'path';
+
+export default {
+    base: '',
+    build: {
+        outDir: 'public/build/',
+        assetsDir: 'public',
+        rollupOptions: {
+            input: {
+                content: path.resolve(__dirname, 'src/assets/js/content.js'),
+                main: path.resolve(__dirname, 'src/assets/js/main.js'),
+                mainCss: path.resolve(__dirname, 'src/assets/css/main.css'),
+                popup: path.resolve(__dirname, 'src/assets/css/popup.css'),
+            },
+
+            output: {
+                assetFileNames: '[name].[ext]',
+                entryFileNames: '[name].js',
+                chunkFileNames: '[name].js',
+            },
+        },
+    },
+    css: {
+        postcss: {},
+    },
+    publicDir: false,
+};
