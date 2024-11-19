1fac807c1b64f0484ca520278a9a9f729b4c1cdc
Ruichimi <theloyworkmail@gmail.com>
2024-11-19 03:45:00 +0100

    Caching without unnecessary info

--- src/assets/js/Services/OsuApiHelper.js
+++ src/assets/js/Services/OsuApiHelper.js
@@ -3,7 +3,8 @@ import axios from "axios";
 class OsuApi {
     async getMapsetData(mapsetId) {
         console.log('Вызванна функция getMapsetData');
-        const cachedData = this.getCachedBeatmapsetData(mapsetId, 'beatmapsetCache', 'beatmapData');
+        const cachedData = this.getCachedBeatmapsetData(mapsetId, 'beatmapsetCache',
+            'beatmapData');
         if (cachedData) {
             console.log('Данные получены из кеша:', cachedData);
             return cachedData;
@@ -11,8 +12,12 @@ class OsuApi {
 
         try {
             const response = await axios.get(`http://localhost:3000/api/MapsetData/${mapsetId}`);
-            console.log('Данные мапсета:', response.data);
-            this.cacheBeatmapsetData(mapsetId, response.data, 'beatmapsetCache', 'beatmapData');
+            const beatmapsData = this.removeExtraInfoFromBeatmapsetDiffs(response.data.beatmaps,
+                ["difficulty_rating", "bpm", "max_combo", "accuracy", "ar", "cs", "drain", "mode"]);
+
+
+            this.cacheBeatmapsetData(mapsetId, response.data, 'beatmapsetCache', 'beatmapData',
+                ["bpm"], {beatmaps: beatmapsData});
             return response.data;
         } catch (error) {
             console.error('Ошибка:', error);
@@ -39,12 +44,25 @@ class OsuApi {
         }
     }
 
-    cacheBeatmapsetData (beatmapId, data, cacheName, cacheItemName) {
+    cacheBeatmapsetData(beatmapId, data, cacheName, cacheItemName, requiredFields, otherParams = {}) {
         const deepParams = JSON.parse(localStorage.getItem(cacheName)) || {};
         const cacheKey = `${cacheItemName}_${beatmapId}`;
-        deepParams[cacheKey] = data;
+        let filteredData = {};
+
+        if (data && typeof data === "object") {
+            requiredFields.forEach(field => {
+                if (field in data) {
+                    filteredData[field] = data[field];
+                }
+            });
+        }
+
+        filteredData = {...filteredData, ...otherParams}
+
+        deepParams[cacheKey] = filteredData;
         localStorage.setItem(cacheName, JSON.stringify(deepParams));
     }
+
 
     getCachedBeatmapsetData(beatmapId, cacheName, cacheItemName) {
         const cacheKey = `${cacheItemName}_${beatmapId}`;
@@ -56,6 +74,25 @@ class OsuApi {
 
         return null;
     }
+
+    removeExtraInfoFromBeatmapsetDiffs(beatmapsetData, requiredFields) {
+        Object.keys(beatmapsetData).forEach((key) => {
+            // Проверяем, является ли значение под этим ключом объектом
+            if (typeof beatmapsetData[key] === 'object' && beatmapsetData[key] !== null) {
+                // Оставляем только поля, указанные в requiredFields
+                beatmapsetData[key] = Object.keys(beatmapsetData[key])
+                    .filter((subKey) => requiredFields.includes(subKey))
+                    .reduce((filteredObject, subKey) => {
+                        filteredObject[subKey] = beatmapsetData[key][subKey];
+                        return filteredObject;
+                    }, {});
+            }
+        });
+
+        return beatmapsetData;
+    }
+
+
 
 }
 
