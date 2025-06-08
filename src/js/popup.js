1d9f6a3865ce3fc84d61335040b086976fce2756
Ruichimi <thekeykeyloy@gmail.com>
2025-06-08 16:04:11 +0200

    Extension window update

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,1 +1,7 @@
-console.log('Last diff info v1.0');
+const issueLink = document.querySelector('.issue-report-link');
+
+issueLink.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.open('https://github.com/Ruichimi/More-Beatmap-Info/issues', '_blank');
+});
+
